859a5aa22eee72eace83d1fb88eb355a57d24bcc
unknown <tanugadalay@gmail.com>
2026-03-04 21:12:50 +0530

    add changes

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if uploaded_file:
     col1, col2 = st.columns(2)
 
     with col1:
-        st.image(image, caption="Uploaded Image", use_container_width=True)
+        st.image(image, caption="Uploaded Image")
 
     with col2:
         if st.button("🔍 Analyze Image"):
