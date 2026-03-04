2adb2ced46552ac293c83a50908247fc241a251a
unknown <tanugadalay@gmail.com>
2026-03-04 20:25:21 +0530

    Switched fully to new google-genai SDK

--- analyzer.py
+++ analyzer.py
@@ -1,13 +1,15 @@
 import os
 from google import genai
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def analyze_image(image):
-    model = genai.GenerativeModel("gemini-1.5-flash-latest")
-
-    response = model.generate_content(
-        ["Analyze this image professionally.", image]
+    response = client.models.generate_content(
+        model="gemini-1.5-flash",
+        contents=[
+            "Analyze this image professionally and describe it in detail.",
+            image
+        ]
     )
 
     return response.text
